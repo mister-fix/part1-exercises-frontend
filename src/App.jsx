846960ddcf3639bcdcd2d27eb48fd738a46ebf6a
Stephen <hellostephenwm@gmail.com>
2024-05-05 21:07:35 -0400

    Added average and positive to stats

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,24 @@ const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const [total, setTotal] = useState(0);
+
+	const average = (good - bad) / total || 0;
+	const positive = (good / total) * 100 || 0;
 
 	const handleGood = () => {
 		setGood(good + 1);
+		setTotal(total + 1);
 	};
 
 	const handleNeutral = () => {
 		setNeutral(neutral + 1);
+		setTotal(total + 1);
 	};
 
 	const handleBad = () => {
 		setBad(bad + 1);
+		setTotal(total + 1);
 	};
 
 	return (
@@ -34,6 +41,9 @@ const App = () => {
 				<p>good {good}</p>
 				<p>neutral {neutral}</p>
 				<p>bad {bad}</p>
+				<p>all {total}</p>
+				<p>average {average}</p>
+				<p>positive {positive}%</p>
 			</div>
 		</div>
 	);
