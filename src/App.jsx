04907629f658028fd4f3a2babec1d328c19a04f7
Stephen <hellostephenwm@gmail.com>
2024-05-05 23:11:07 -0400

    Extracted statistics to own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import { useState } from "react";
 
+const Statistics = ({ good, neutral, bad }) => {
+	const total = good + neutral + bad;
+
+	return (
+		<div>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
+			<p>all {good + neutral + bad}</p>
+			<p>average {`${(good - bad) / total || 0}`}</p>
+			<p>positive {`${(good / total) * 100 || 0}`}%</p>
+		</div>
+	);
+};
+
 const App = () => {
-	// save clicks of each button to its own state
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
-	const [total, setTotal] = useState(0);
-
-	const average = (good - bad) / total || 0;
-	const positive = (good / total) * 100 || 0;
 
 	const handleGood = () => {
 		setGood(good + 1);
-		setTotal(total + 1);
 	};
 
 	const handleNeutral = () => {
 		setNeutral(neutral + 1);
-		setTotal(total + 1);
 	};
 
 	const handleBad = () => {
 		setBad(bad + 1);
-		setTotal(total + 1);
 	};
 
 	return (
@@ -37,14 +44,11 @@ const App = () => {
 
 			<h2>statistics</h2>
 
-			<div>
-				<p>good {good}</p>
-				<p>neutral {neutral}</p>
-				<p>bad {bad}</p>
-				<p>all {total}</p>
-				<p>average {average}</p>
-				<p>positive {positive}%</p>
-			</div>
+			<Statistics
+				good={good}
+				neutral={neutral}
+				bad={bad}
+			/>
 		</div>
 	);
 };
