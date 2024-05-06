f5a3db67a74f042d47d8bcfd6bca292f87865600
Stephen <hellostephenwm@gmail.com>
2024-05-05 21:02:38 -0400

    Implemented feedback buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const App = () => {
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	const handleGood = () => {
+		setGood(good + 1);
+	};
+
+	const handleNeutral = () => {
+		setNeutral(neutral + 1);
+	};
+
+	const handleBad = () => {
+		setBad(bad + 1);
+	};
+
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<h2>give feedback</h2>
+
+			<div>
+				<button onClick={handleGood}>good</button>
+				<button onClick={handleNeutral}>neutral</button>
+				<button onClick={handleBad}>bad</button>
+			</div>
+
+			<h2>statistics</h2>
+
+			<div>
+				<p>good {good}</p>
+				<p>neutral {neutral}</p>
+				<p>bad {bad}</p>
+			</div>
 		</div>
 	);
 };
